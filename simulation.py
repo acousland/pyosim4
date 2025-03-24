175a586fabf9533764913cc8b39a789f674ede3a
Aaron Cousland <acousland@gmail.com>
2025-03-24 20:02:43 +1100

    Updated with single step changes.

--- simulation.py
+++ simulation.py
@@ -232,6 +232,7 @@ class Parameters:
         self.mutation_rate = 0.01
         self.survivor_fraction = 0.2
         self.visualise_interval = 2
+        self.intergeneration_mode = False
         
         # Load values from config file if provided
         if config_file:
@@ -259,16 +260,21 @@ class Simulation:
         self.ax2.set_facecolor('white')  # Set the axes background color to white
         self.generation = 0
 
+    def run_step(self):
+        for indiv in self.population:
+            if indiv.alive:
+                sensors = [indiv.get_sensor_value(i, self.signals, self.population) for i in range(Sensor.NUM_SENSES.value)]
+                actions = indiv.nnet.feed_forward(sensors)
+                indiv.execute_actions(actions, self.signals, self.population)
+        alive_count = sum(1 for indiv in self.population if indiv.alive)
+        print(f"step: Alive {alive_count}/{self.params.population}")
+        self.signals.fade()
+
     def run_generation(self):
         for _ in range(self.params.steps_per_generation):
-            for indiv in self.population:
-                if indiv.alive:
-                    sensors = [indiv.get_sensor_value(i, self.signals, self.population) for i in range(Sensor.NUM_SENSES.value)]
-                    actions = indiv.nnet.feed_forward(sensors)
-                    indiv.execute_actions(actions, self.signals, self.population)
-            alive_count = sum(1 for indiv in self.population if indiv.alive)
-            print(f"step: Alive {alive_count}/{self.params.population}")
-            self.signals.fade()
+            self.run_step()
+        self.reproduce()
+        self.generation += 1
 
     def reproduce(self):
         print("Reproducing")
@@ -315,16 +321,18 @@ class Simulation:
         #        print(f"Individual at ({indiv.loc.x}, {indiv.loc.y})")
 
     def animate(self, _):
-        self.run_generation()
-        if self.generation % self.params.visualise_interval == 0:
+        #self.run_generation()
+        self.run_step()
+        #if self.generation % self.params.visualise_interval == 0:
+        if True:
             self.log_stats()
             self.visualise()
-        self.reproduce()
-        self.generation += 1
+        #self.reproduce()
+        #self.generation += 1
 
 # --- Entry Point ---
 if __name__ == "__main__":
     sim = Simulation('config.json')
-    anim = FuncAnimation(sim.fig, sim.animate, frames=generations, repeat=False, blit=False)
+    anim = FuncAnimation(sim.fig, sim.animate, frames=generations*sim.params.steps_per_generation, repeat=False, blit=False)
     plt.show()
 
